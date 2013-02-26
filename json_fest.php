35f7e3956bd752597e1be32948a1133ae4b6d7ec
Pravendra Singh <hackpravj@gmail.com>
2013-02-27 00:57:01 +0530

    now working fine with ['] in album names and song names also

--- json_fest.php
+++ json_fest.php
@@ -1,12 +1,14 @@
 <?php
-  $album = $_POST["album"];
+        $album = $_POST["album"];
 	$song = $_POST["song"];
+	$alb = "$album";
+	$sng = "$song";
 	$maindata = file_get_contents("http://sdslabs.co.in/muzi/ajax/album/list.php");
 	$m_stg = substr_count($maindata,"language");
 	$mdsolve = json_decode($maindata,true);
 	for($i=0;$i<$m_stg;$i++)
 	{
-	    if(strcmp($album,$mdsolve[$i]['name'])==0)
+	    if(strcmp($alb,$mdsolve[$i]['name'])==0)
 		{
 		    $albumId = $mdsolve[$i]['id'];
 		    $albumdata = file_get_contents("http://www.sdslabs.co.in/muzi/ajax/album?id=$albumId;");
@@ -14,7 +16,7 @@
 			$adsolve = json_decode($albumdata,true);
 			for($j=0;$j<$a_stg;$j++)
 			{
-			    if(strcmp($song,$adsolve["tracks"][$j]["title"])==0)
+			    if(strcmp($sng,$adsolve["tracks"][$j]["title"])==0)
 				{
 				    $trackId = $adsolve["tracks"][$j]["id"];
 					$trackdata = file_get_contents("http://www.sdslabs.co.in/muzi/ajax/track?id=$trackId");
